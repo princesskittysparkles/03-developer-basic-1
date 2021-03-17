108135eb2c8cc95826d97668949aea9ed33b188d
Frank Lamar <lamar-frankie@users.noreply.github.com>
2021-03-17 22:15:08 +0000

    added boilerplate

---
+++ password.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3 
+
+#Program Notes
+#TODO Fill out the program notes
+"""
+Aurthor : [Your name here]
+Email   : [Your email address here]
+Purpose : [What does this program do?]
+Date    : [Date of completion]
+"""
+
+#TODO create a list of upper case characters
+
+
+#TODO create a list of lowercase characters
+
+
+#TODO create a list of number
+
+
+#TODO create a list of symbols
+
+
+#TODO ask user for desired password length
+
+
+#TODO ask user if they want to include symbols in their password
+
+
+#TODO generate password according to user specs and give it to user
+
+### BONUS ###
+##TODO automatically copy password to the users clipboard
+
---
+++ test_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Tests for password.py"""
+
+
+### This section is optional ###
+
+#You can complete this section if you are familiar with testing in python or Test driven development
+
+#TODO: Test that password.py exists
+
+
+#TODO: Test that password.py is executable 
+
+
+#TODO: Test that password generated is length user specified
+
+
+#TODO: Test that password contains the characters that the user specified
+
+
+#TODO: Add any other test that you would like
